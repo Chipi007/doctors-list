0a9bd6d6311a7ce9de75bbecba1e8890b723f33d
Alina Konuhova <akonuhova16@gmail.com>
2022-09-22 14:01:29 +0300

    Change experience validation in the validation.js

--- src/components/inputFileGroup/InputFileGroup.jsx
+++ src/components/inputFileGroup/InputFileGroup.jsx
@@ -4,7 +4,7 @@ import s from './inputFileGroup.module.scss'
 export const InputFileGroup = ({textBtn, handleChange, photoName}) => {
   return (
     <div className={s.inputGroup}>
-        <label>Фото</label>
+        <label className={s.label}>Фото</label>
         <label htmlFor = 'file' className = {s.uploadButton}>{textBtn} фото</label>
         <input name = 'photo' className={s.hiddenButton} id = 'file' type = 'file' accept='image/*' onChange={e => handleChange(e)}/>
         <span className={s.span} htmlFor="file">{photoName}</span>
--- src/components/modal/validation.js
+++ src/components/modal/validation.js
@@ -2,14 +2,15 @@ import * as Yup from 'yup';
 
 const LettersError = 'Введите не меньше 5 буквенных символов';
 const emptyError = 'Поле не может быть пустым';
-const NumbersError = 'Введите не больше 5 числовых символов';
+const NumbersError5 = 'Введите не больше 5 числовых символов';
+const NumbersError2 = 'Введите не больше 2 числовых символов';
 
 export const validationSchema = Yup.object({
     fio: Yup.string().matches(/^([A-Za-zЁёА-я ]){5,50}$/, LettersError).required(emptyError),
     profession: Yup.string().matches(/^([A-Za-zЁёА-я ]){5,50}$/, LettersError).required(emptyError),
     hospital: Yup.string().matches(/^([A-Za-zЁёА-я ]){5,50}$/, LettersError).required(emptyError),
-    experience: Yup.string().matches(/^(\d){1,5}$/g, NumbersError).required(emptyError),
+    experience: Yup.string().matches(/^(\d){1,2}$/g, NumbersError2).required(emptyError),
     education: Yup.string().matches(/^([A-Za-zЁёА-я ]){5,50}$/, LettersError).required(emptyError),
     address: Yup.string().required(emptyError),
-    price: Yup.string().matches(/^(\d){1,5}$/g, NumbersError).required(emptyError)
+    price: Yup.string().matches(/^(\d){1,5}$/g, NumbersError5).required(emptyError)
 })
